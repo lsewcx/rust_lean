5a581b25d62624d0833aecb07073707b9b4c5d09
lsewcx <lse210085@163.com>
2026-08-25 17:33:03 +0800

    done

--- main.rs
+++ main.rs
@@ -1,12 +1,18 @@
 use std::io::{self, BufRead};
 
-fn count(s: &str) -> usize {
-
-    return s.len();
-}
-
 fn main() {
     let stdin = io::stdin();
     let line = stdin.lock().lines().next().unwrap().unwrap();
-    println!("{}", count(&line));
+    let nums: Vec<i64> = line.split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+    // Find and print the maximum.
+    let _ = nums;
+    let mut result=std::i64::MIN;
+    for i in nums {
+        if i > result {
+            result = i;
+        }
+    }
+    println!("{}", result);
 }
