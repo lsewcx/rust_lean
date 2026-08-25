79049406ac73f6565af49f1bb678349e503ddd55
lsewcx <lse210085@163.com>
2026-08-25 15:29:54 +0800

    done

--- main.rs
+++ main.rs
@@ -2,6 +2,10 @@ use std::io::{self, BufRead};
 
 fn main() {
     let stdin = io::stdin();
-    let line = stdin.lock().lines().next().unwrap().unwrap();
-    println!("{}", line.to_uppercase());
+    let mut iter = stdin.lock().lines();
+    let name = iter.next().unwrap().unwrap();
+    let age: i32 = iter.next().unwrap().unwrap().trim().parse().unwrap();
+    // Print the greeting.
+    let _ = (&name, age);
+    println!("Hi, {}! You are {} years old.", name, age);
 }
